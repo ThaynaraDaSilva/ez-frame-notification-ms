a541cd45bfe7957fe32faa376e545c6eef21d061
tchfer <tchfernando@gmail.com>
2025-04-12 16:46:29 -0300

    Ajustar estrutura do projeto

--- src/main/java/br/duosilva/tech/solutions/ez/frame/notification/ms/adapters/in/controller/NotificationController.java
+++ src/main/java/br/duosilva/tech/solutions/ez/frame/notification/ms/adapters/in/controller/NotificationController.java
@@ -2,7 +2,6 @@ package br.duosilva.tech.solutions.ez.frame.notification.ms.adapters.in.controll
 
 import br.duosilva.tech.solutions.ez.frame.notification.ms.application.usecases.NotificationUsecase;
 import br.duosilva.tech.solutions.ez.frame.notification.ms.application.dto.NotificationRequest;
-import br.duosilva.tech.solutions.ez.frame.notification.ms.domain.model.Notification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,13 +20,7 @@ public class NotificationController {
 
     @PostMapping("/send")
     public ResponseEntity<String> sendNotification(@RequestBody NotificationRequest request) {
-        Notification notification = new Notification(
-                request.getVideoId(),
-                request.getStatus(),
-                request.getErrorMessage(),
-                request.getEmail()
-        );
-        notificationUsecase.sendNotification(notification);
+        notificationUsecase.sendNotification(request);
         return ResponseEntity.ok("Notificação enviada com sucesso");
     }
 }
